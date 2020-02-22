8af5a993c3b08b8cb74d9a54e1dd63ff07d48352
arepoque1992 <ruiming.kang@gmail.com>
2020-02-22 16:34:27 +0800

    更改了product1.vue中获取数组索引的方式。
    Signed-off-by: arepoque1992 <ruiming.kang@gmail.com>

--- src/components/productsSub/Product1.vue
+++ src/components/productsSub/Product1.vue
@@ -25,8 +25,8 @@
 				<div class="row">
 					<h2 style="text-align: left;"><b>产品特点</b></h2>
 				</div>
-				<div class="row" v-for="featureItem in productFeature" :key="featureItem">
-					<h3><br>特点 {{featureItem.index}}:</h3>
+				<div class="row" v-for="(featureItem, index) in productFeature" :key="featureItem"><!--vue2.0 get index from array-->
+					<h3><br>特点 {{index + 1}}:</h3>
 					<h4 v-html="featureItem.content"></h4>	<!--this line is used to convert the varible value to html code, typically be used to implement line change.-->
 					<img class="col-md-12" :src="featureItem.itemPic" />
 				</div>
@@ -59,23 +59,19 @@
 					content: '本系统通过以太网交换机可以实现5万(50,000)测量通道以上的数据同步采集，其部分核心技术指标超过国外同类产品。',
 				}],
 				productFeature: [{
-					index: '1',
 					content: '系统采用分布式结构，采集前端在被测点附近完成被测信号的数字化，通过CAN总线将采集数据传输至CAN服务器，再经由以太网将数据汇总在应用服务器进行' +
 							 '显示和分析。采集前端距CAN服务器最远距离可达150米。<br><br>分布式结构使得采集系统的模拟线缆由原来的数十米缩短至2米，甚至更短，充分保证了测' +
 							 '量精度。此外，通过对测量信号的自动编址，极大地降低了信号人工编址的工作量，提高了工作效率，也在很大程度上缩短了实验的准备时间。<br>',
 				},
 				{
-					index: '2',
 					content: '采集前端由4个V-F测量模块及其外围电路组成，实现16路应变量的高精度测量。系统通过测试放电时间来间接得出电阻应变，芯片首先测量两个桥臂电阻的放' +
 							 '电时间，根据两次放电时间的比值，计算出应变电阻的变化情况。<br>',
 					itemPic: require('../../assets/products/product1/schematic.jpg'),
 				},
 				{
-					index: '3',
 					content: '系统采用三层架构的方式，通过专用的总线间同步和总线内同步协议，解决分布式系统下大批量被测信号的同步问题，使全部采集通道实现实时同步采集。<br>',
 				},
 				{
-					index: '4',
 					content: '系统采用多样化显示方式，提供波形显示、数据表以及云图等显示模式，可利用色彩反映应力变化并进行预警，以便于及时发现实验中的异常情况，将事后数据' +
 							 '分析的实验方式变为跟踪整个实验的过程，优化实验方式。',
 				}],
