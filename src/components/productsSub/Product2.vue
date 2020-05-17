a1d060132b606375dd518d27310cb58378d6db62
arepoque1992 <ruiming.kang@gmail.com>
2020-05-17 13:27:14 +0800

    2020.5.17小改动

--- src/components/productsSub/Product2.vue
+++ src/components/productsSub/Product2.vue
@@ -9,7 +9,10 @@
 				</div>
 				<TopBlank></TopBlank>
 				<div class="row">
-					<img class="col-md-12" :src="productInfo.pPic" />
+					<div class="md-col-12" v-for="(picItem, index) in productInfo.picList" :key="picItem.index">
+						<img class="col-md-12" :src="picItem.pic" />
+					</div>
+					
 				</div>
 			</div>
 			<div class="col-md-1"></div>
@@ -29,7 +32,14 @@
 				productInfo: {
 					pTitle: '西安航空学院静力试验平台',
 					pProfile: '我司于2019年下半年为西安航空学院提供一套航空部件静力试验教学平台。',
-					pPic: require('../../assets/products/product2/pic1.jpg'),
+					picList: [{
+						pic: require('../../assets/products/product2/pic1.jpg')
+					},
+					// {
+					// 	pic: require('../../assets/products/product2/pic1.jpg')
+					// },
+					],
+					// pPic: require('../../assets/products/product2/pic1.jpg'),
 					// pVideo: require('../assets/products/product2/acquisitionUnit.mp4')
 				}
 			}
